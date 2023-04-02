64f392f242783366b29fbf0aaedb9242e8698b6e
polespinasa <polregisters@gmail.com>
2023-04-02 12:50:59 +0200

    First automatic attack -- SUCCESS!

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from conf import *
 from DockerManager import DockerManager
+from datetime import datetime
 
 
 #INITIAL GLOBAL VARIABLES
@@ -9,88 +10,84 @@ NODE_LIST = DOCKER_MANAGER.nodes
 
 def main():
 
+	#SIMPLE CONTRACT BREACH ATTACK SIMULATION
 
 
-	#THIS MAIN IS BEING USED FOR TESTING PURPOSES
-
-
-
-	#USE NODE_LIST to interact with the different nodes
-	#rawTX = ''
+	# 1. Identify all nodes and save in easy variables to use
 	for i in NODE_LIST:
-
-		#result = i.forceLinuxCommand('ls /home')
-
-		#TEST BITCOIN CORE CALLS -- WORKING
-		'''
-		if i.implementation == 'bitcoind':
-			result =  i.getBlockchainInfo()
-			print('Node Name: ' + i.name + ' | Node implementation: ' + i.implementation)
-			print(result['chain'])
-
-			result = i.sendRawTransaction('0200000000010103b13e7b5c596858c2aa45e31f9fd8eee9ca72c29bebbaeb3abff6edcada5c770100000000d7d6d0800210270000000000002200200ac3d06c10f477f9a61b26dfaacea322092ee9ecad5c47252c6688d543fc9482268603000000000016001423b23606aedf19f4e6d19cd44274c0363f3d972a04004730440220417bf1591bb6273abb92055bcbd0b312679d95f250fda4c60ea5bfa584e207b9022039240299b27d713fbf663ef214b62b3720a2b2e0b47636630bf049cbac1ab3bb01483045022100fc27f3d3fb1043a19cf187481b6c7317e83402c1c484639f46d96ca709464ae80220610b7019094ec71d89094969d8405f3b4a3cb665a221877e037000e3b21187460147522102fea906d4718f8bcffc9c037fa3e737a59372b21e9881d6fcd011f65d5e2e5a2721031f024390005362c104cfc04c72d90fdeff04abe849d655560e05d2a48d083deb52ae75cd8d20')
-			print(result)
-		'''
-
-		'''
-		peerId = ''
-		#TEST C-LIGHTNING CALLS -- WORKING
-		if i.implementation == 'lightningd':
-			result = i.getPeersIds()
-			peerId = result[0]
-
-		#TEST C-LIGHTNING CALLS RPC -- WORKING
-		if i.implementation == 'lightningd':
-			result = i.signLastTx(peerId)
-			#print(result)
-			rawTX = result
-			
-		
-
-		#TEST C-LIGHTING PAY BOLT11 -- WORKING
-		bolt11 = 'lnbcrt500u1pjzsnsmpp55c33agyhl835j4th2ffl6hygx3usj3e548tr8kgr0njm547jvl3sdqqcqzpgxqyz5vqsp5t99v9yyefvdapk6ytwymnev0arp5cj4h6fgkxcc3s8rvxyclmdas9qyyssq8cwmla8zmsr8xrkpu5wc8wfsluk69zuelhq09scsewqy4u2kpk754lfgs0sj33e0w097fankvxgtmep9qe2dspvmjmcyj9kg0uymnfsqvztru0'
-		if i.implementation == 'lightningd':
-			result = i.payInvoice(bolt11)
-			print(result)
-		'''
-
-		'''
-		#TEST C-LIGHTING CREATION BOLT11 -- WORKING
-		if i.implementation == 'lightningd':
-			result = i.createInvoice(1000)
-			print(result)
-			print('\n')
-			result = i.createInvoice(5)
-			print(result)
-			print('\n')
-			result = i.createInvoice(0)
-			print(result)
-		'''
-
-		'''
-		#TEST LND CREATION BOLT 11 -- working
 		if i.implementation == 'lnd':
-			result = i.createInvoice(100000)
-			print(result)
-		'''
-
-		'''
-		#TEST LND PAYMENT BOLT 11 -- working
-		bolt11 = 'lnbcrt1m1pjzj5mfsp56eqedhjffut76apfpcpddwpdh4kftradd7cz5v4s2e9vsdx5hqdspp5x0lmjr2cpa6hh7w4s2mjzxu3ktkjnyf9w6a2j3gyqzmzfsmh2kcsdpz2phkcctjypykuan0d93k2grxdaezqcn0vgxqyjw5qcqp29qyysgqsh26lkpg7zafwz645ql0qx3hdsmvqwqastx0j37p2gfx7y8h0fljfj2kmd3hp7l56hg2j0d0z2syueruf972h4kr3ulagj06es55y8qpg6c24d'
-		if i.implementation == 'lnd':
-			result = i.payInvoice(bolt11)
-			print(result)
-		'''
+			victim = i
+		elif i.implementation == 'lightningd':
+			malicious = i
+		else:
+			bitcoin_core = i
 
 
-	'''		
-	#TEST BROADCAST COMMITMENT TRANSACTION -- WORKING
-	for i in NODE_LIST:
-		if i.implementation == 'bitcoind':
-			print(rawTX)
-			result = i.sendRawTransaction(rawTX)
-			print(result)
-	'''
+	
+	# 2. Initialize the results file
+	date = datetime.now()
+	file_name = str(date.day) + '_' + str(date.month) + '_' + str(date.hour) + str(date.minute) + str(date.second) + '.txt'
+	f = open(file_name, "w")
+	f.write("SIMULTAION TIME " + str(date) +'\n')
+
+
+	### MAYBE CREATE A CLASS THAT DOES ALL THIS JOB AUTOMATICALLY
+	victimBalance = 10045
+	maliciousBalance = 239955
+	f.write("INITIAL STATE:\n")
+	f.write("Victim balance: " + str(victimBalance) + '\n')
+	f.write("Malicious balance: " + str(maliciousBalance) + '\n')
+
+	
+	# 3. Malicious pays 100k sats to victim
+
+	invoice = victim.createInvoice(100000)
+	res = malicious.payInvoice(invoice)
+	if res:
+		print('Payment completed')
+
+	victimBalance += 100000
+	maliciousBalance -= 100000
+	f.write("\n\nMalicious pays 100k sats to victim")
+	f.write("  Victim balance: " + str(victimBalance) + "\n")
+	f.write("  Malicious balance: " + str(maliciousBalance) + "\n")
+
+
+	# 4. Victim pays 50k sats to malicious
+	invoice = malicious.createInvoice(50000)
+	res = victim.payInvoice(invoice)
+	if res:
+		print('Payment completed')
+
+	victimBalance -= 50000
+	maliciousBalance += 50000
+	f.write("\n\nVictim pays 50k sats to malicious")
+	f.write("  Victim balance: " + str(victimBalance) + "\n")
+	f.write("  Malicious balance: " + str(maliciousBalance) + "\n")	
+
+	# 5. Malicious save this commitment transaction
+	peerId = malicious.getPeersIds()[0]
+	commitmentTx = malicious.signLastTx(peerId)
+	f.write("  Commitment raw TX: \n   " + commitmentTx)
+
+
+	#6. Malicous pays 40k sats to victim
+	invoice = victim.createInvoice(40000)
+	res = malicious.payInvoice(invoice)
+	if res:
+		print('Payment completed')
+
+	victimBalance += 40000
+	maliciousBalance -= 40000
+	f.write("\n\nMalicious pays 50k sats to victim")
+	f.write("  Victim balance: " + str(victimBalance) + "\n")
+	f.write("  Malicious balance: " + str(maliciousBalance) + "\n")	
+
+	#7. Malicious broadcast previous state
+	res = bitcoin_core.sendRawTransaction(commitmentTx)
+	f.write("\n\nMalicious broadcast previous state")
+	f.write("  Commitment transaction in hex: \n  " + res)
+
 
 
 
